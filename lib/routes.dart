d26740a8fa154d9837dd00d46937e9310d529c34
brocode <kelimbth@gmail.com>
2024-06-26 11:05:41 +0300

    first commit 🚀 - created route for homepage screen

---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/screens/homepage/homepage.dart';
+
+
+final Map<String, WidgetBuilder> routes = {
+  Homepage.routeName: (context) => const Homepage(),
+  
+};
