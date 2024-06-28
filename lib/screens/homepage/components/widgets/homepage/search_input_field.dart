88e83800de5ea93242d31a549762285943ae49a0
brocode <kelimbth@gmail.com>
2024-06-28 13:13:30 +0300

    created a search input field to query groceries

---
+++ lib/screens/homepage/components/widgets/homepage/search_input_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/constants/colors.dart';
+
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+              offset: const Offset(0, 12),
+              blurRadius: 15,
+              color: kBoxShadowColor.withOpacity(0.6))
+        ],
+        color: Colors.white,
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Search your daily grocery food',
+          hintStyle: TextStyle(color: kHintTextandPrefixIconColor),
+          prefixIcon: Icon(Icons.search),
+          prefixIconColor: kHintTextandPrefixIconColor,
+          suffixIcon: Icon(Icons.filter_list),
+          suffixIconColor: kHintTextandPrefixIconColor,
+        ),
+      ),
+    );
+  }
+}
