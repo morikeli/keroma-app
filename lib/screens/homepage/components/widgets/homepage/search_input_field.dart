96e3858f8bbf696a4e8f27441b31c03505cc0712
brocode <kelimbth@gmail.com>
2024-06-28 15:57:18 +0300

    converted SearchField to a stateful widget

--- lib/screens/homepage/components/widgets/homepage/search_input_field.dart
+++ lib/screens/homepage/components/widgets/homepage/search_input_field.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/constants/colors.dart';
 
-class SearchField extends StatelessWidget {
+
+class SearchField extends StatefulWidget {
   const SearchField({
     super.key,
   });
 
+  @override
+  State<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
   @override
   Widget build(BuildContext context) {
     return Container(
