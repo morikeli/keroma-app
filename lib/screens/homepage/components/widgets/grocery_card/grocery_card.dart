e87b2af262a24d3039f9743a5bb5178ed38bdbf4
brocode <kelimbth@gmail.com>
2024-07-01 13:49:48 +0300

    wrapped Container() widget in an InkWell()

--- lib/screens/homepage/components/widgets/grocery_card/grocery_card.dart
+++ lib/screens/homepage/components/widgets/grocery_card/grocery_card.dart
@@ -4,6 +4,7 @@ import 'package:grocery_app/screens/homepage/components/widgets/grocery_card/com
 import 'package:grocery_app/screens/homepage/components/widgets/grocery_card/components/grocery_card_image.dart';
 import 'package:grocery_app/screens/homepage/components/widgets/grocery_card/components/grocery_card_subtitle.dart';
 import 'package:grocery_app/screens/homepage/components/widgets/grocery_card/components/grocery_card_title.dart';
+import 'package:grocery_app/screens/product_details/product_details.dart';
 
 
 class GroceryCard extends StatelessWidget {
@@ -13,20 +14,23 @@ class GroceryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 163,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
-          color: Colors.grey.shade300.withOpacity(.4)),
-      child: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          GroceryCardDiscountBanner(),
-          GroceryCardImageWidget(),
-          GroceryCardTitleWidget(),
-          GroceryCardSubtitleWidget(),
-          GroceryCardFooter(),
-        ],
+    return InkWell(
+      onTap: () => Navigator.pushNamed(context, ProductDetailsScreen.routeName),
+      child: Container(
+        width: 163,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+            color: Colors.grey.shade300.withOpacity(.4)),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GroceryCardDiscountBanner(),
+            GroceryCardImageWidget(),
+            GroceryCardTitleWidget(),
+            GroceryCardSubtitleWidget(),
+            GroceryCardFooter(),
+          ],
+        ),
       ),
     );
   }
