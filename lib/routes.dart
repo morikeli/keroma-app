024f5d1cc7406e2620b6d0cbc4cfdaa43a081566
brocode <kelimbth@gmail.com>
2024-07-01 13:54:08 +0300

    added new route name

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/screens/homepage/homepage.dart';
+import 'package:grocery_app/screens/product_details/product_details.dart';
 
 
 final Map<String, WidgetBuilder> routes = {
   Homepage.routeName: (context) => const Homepage(),
-  
+  ProductDetailsScreen.routeName: (context) => const ProductDetailsScreen(), 
 };
