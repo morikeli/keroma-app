5a1ac58a318e9ae672004a18f4bfc2e6ff35cb38
brocode <kelimbth@gmail.com>
2024-07-02 10:12:08 +0300

    colors used in the application

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+const kPrimaryColor = Color(0xFF0BA310);
+const kTextLightColor = Color(0xFFFFFFFF);
+const kTextDarkColor = Color(0xFF000000);
+const kTextGreyColor = Colors.grey;
+const kIconLightColor = Color(0xFFFFFFFF);
+const kHintTextandPrefixIconColor = Color(0xFF9E9E9E);
+const kCartBadge = Color(0xFFFF9800);
+const kBodyBackgroundColor = Color(0xFFEEEEEE);
+const kFruitsBannerColor = Color(0xFFFFEB3B);
+const kScaffoldBackgroundColor = Color(0xFFF5F4F4);
+const kBoxShadowColor = Color(0xFFDADADA);
