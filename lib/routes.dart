d935ac851d9a882b2f6fc2c2d7d149fedc7945be
brocode <kelimbth@gmail.com>
2024-07-03 09:43:48 +0300

    appended new route names

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/screens/homepage/homepage.dart';
+import 'package:grocery_app/screens/login/login.dart';
+import 'package:grocery_app/screens/otp/otp.dart';
 import 'package:grocery_app/screens/product_details/product_details.dart';
+import 'package:grocery_app/screens/signup/signup.dart';
 
 
 final Map<String, WidgetBuilder> routes = {
+  LoginScreen.routeName: (context) => const LoginScreen(),
+  SignupScreen.routeName:(context) => const SignupScreen(), 
+  OTPScreen.routeName:(context) => const OTPScreen(),
   Homepage.routeName: (context) => const Homepage(),
   ProductDetailsScreen.routeName: (context) => const ProductDetailsScreen(), 
 };
