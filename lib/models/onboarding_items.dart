3305f845fadb4752f552ec8249d2bf1c8491a171
brocode <kelimbth@gmail.com>
2024-07-04 12:30:16 +0300

    created a list of items shown in the onboarding screen

---
+++ lib/models/onboarding_items.dart
@@ -0,0 +1,17 @@
+List<Map<String, String>> items = [
+  {
+    "text": "Buy online groceries",
+    "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, numquam omnis aspernatur illo tenetur beatae labore impedit eveniet, explicabo voluptates eaque sint qui odit error fugiat ex sunt asperiores quae.",
+    "image": 'assets/imgs/fruits/orange.png',
+  },
+  {
+    "text": "Helathy meal, healthy life",
+    "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, numquam omnis aspernatur illo tenetur beatae labore impedit eveniet, explicabo voluptates eaque sint qui odit error fugiat ex sunt asperiores quae.",
+    "image": 'assets/imgs/fruits/orange.png',
+  },
+  {
+    "text": "Organic food",
+    "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, numquam omnis aspernatur illo tenetur beatae labore impedit eveniet, explicabo voluptates eaque sint qui odit error fugiat ex sunt asperiores quae.",
+    "image": 'assets/imgs/fruits/orange.png',
+  },
+];
