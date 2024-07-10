af24791d40c3cfe181a89ec4e79819f6e6943489
brocode <kelimbth@gmail.com>
2024-07-10 11:56:10 +0300

    created a body widget for signup screen

---
+++ lib/screens/signup/signup_body.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/screens/signup/components/signup_form.dart';
+
+
+class SignupScreenBody extends StatelessWidget {
+  const SignupScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * .04),
+                const Text(
+                  'Create an account', 
+                  style: TextStyle(color: Colors.black, fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  'Enter your email address and password to create an account today', 
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * .08),
+                const SignupForm(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
